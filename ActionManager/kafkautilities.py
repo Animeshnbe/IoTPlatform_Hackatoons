a2ba7d288ec4d85a7acefe0039025c25bd799ab2
wolfblunt <amankhandelwaljuly@gmail.com>
2023-04-07 20:44:44 +0530

    Utility Change

--- ActionManager/kafkautilities.py
+++ ActionManager/kafkautilities.py
@@ -2,15 +2,25 @@ from kafka import KafkaProducer
 import json
 from kafka import KafkaConsumer
 
-def Produce(ip,port,topic,message):
-    producer = KafkaProducer(bootstrap_servers=[ip+":"+port])
-    producer.send(topic,value = json.dumps(message).encode('utf-8'))
-def Consume(ip,port,offset,topiclist):
-    consumer = KafkaConsumer(bootstrap_servers=[ip+":"+port],auto_offset_reset=offset,enable_auto_commit=True,value_deserializer=lambda x: x.decode('utf-8'))
-    consumer.subscribe(topics=topiclist)
-    responses = []
-    for message in consumer:
-        responses.append(message.value)
-    consumer.close()
+
+def kafka_produce(ip, port, topic, message):
+    try:
+        producer = KafkaProducer(bootstrap_servers=[ip + ":" + port])
+        producer.send(topic, value=json.dumps(message).encode('utf-8'))
+    except Exception as e:
+        print(e)
 
 
+def kafka_consume(ip, port, offset, topiclist):
+    try:
+        consumer = KafkaConsumer(bootstrap_servers=[ip + ":" + port], auto_offset_reset=offset, enable_auto_commit=True,
+                                 value_deserializer=lambda x: x.decode('utf-8'))
+        consumer.subscribe(topics=topiclist)
+        responses = []
+        for message in consumer:
+            responses.append(message.value)
+        consumer.close()
+        return responses
+    except Exception as e:
+        print(e)
+        return "error"
